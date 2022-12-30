5d3030287fb45279ff20db42600164061825d6e2
Amir Hossein Miarasadollahnejad <e1426427@student.tuwien.ac.at>
2022-12-30 20:25:25 +0100

    Add python script to create bar charts for each property

---
+++ experiments-plot.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+import argparse
+import seaborn as sns
+
+
+path = os.path.abspath('')
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--file", help="csv file to be used")
+parser.add_argument("--function", help="Function which have been used")
+parser.add_argument("--toPlot", help="The property to plot")
+parser.add_argument("--method", help="Either rejection method or penalty method")
+
+args = parser.parse_args()
+
+print(f"Given data: {vars(args)}")
+
+file = args.file
+function = args.function
+to_plot = args.toPlot
+method = args.method
+
+file = path + '/' + file
+filename = os.fsdecode(file)
+data = pd.read_csv(filename, header=6)
+
+df = pd.DataFrame(data)
+
+sns.barplot(data=df, y=list(df['global-best-val']), x=list(df[to_plot]), errorbar=None)
+plt.title("Global best value depending on " + to_plot)
+plt.xlabel(to_plot)
+plt.ylabel("Global best value")
+
+plt.show()
+
